85dcc85a0f3252f539dbd7cf7b4e49557d9fd289
Diana Wanjiru <karwanjiru@gmail.com>
2024-06-22 20:21:43 +0300

    fixed error

---
+++ api_call_example/my_actions/reverse_string.py
@@ -0,0 +1,22 @@
+from asyncflows import Action, BaseModel, Field
+
+import aiohttp
+
+
+class Inputs(BaseModel):
+    input_string: str
+
+
+class Outputs(BaseModel):
+    reversed_string: str
+
+
+class MyReverseString(Action[Inputs, Outputs]):
+    name = 'my_reverse_string'
+
+    async def run(self, inputs: Inputs) -> Outputs:
+        return Outputs(
+            reversed_string=inputs.input_string[::-1]
+    )
+
+
